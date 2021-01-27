c34009ac7f47dd1109e51a1f692307db2b7c2931
Shankari <shankari@eecs.berkeley.edu>
2021-01-26 22:14:02 -0800

    Fix/modify the setup scripts
    - add an activate option to quickly activate the environment instead of setting everything up
    - change the setup code to install emission and the emission viz/notebook code
    - ensure that we install/activate conda as well

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+pushd $EMISSION_SERVER_HOME
+source setup/checks/check_for_conda.sh
+popd
+
 conda env update --name emission-private-eval --file $EMISSION_SERVER_HOME/setup/environment36.yml
 conda env update --name emission-private-eval --file $EMISSION_SERVER_HOME/setup/environment36.notebook.additions.yml
 source activate emission-private-eval
