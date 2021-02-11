966284e62b1614a4f932d62e151d4104e133a63f
Chunrui Huang <corinnehcr@gmail.com>
2021-02-10 18:43:49 -0800

    cleaned up the codes, merged viz functions, added comments

--- tour_model_eval/viz_bin_cluster.py
+++ tour_model_eval/viz_bin_cluster.py
@@ -15,108 +15,86 @@ import folium
 import branca.colormap as cm
 
 
-def bins_map(bins, trips):
+def bins_map(bins, bins_list, trips):
     # Plot all the bin trips on the map, use different colors for different bins.
     # Each color represents one bin based on index
     # Choose the first start point from the trips to locate the map for convenience
-    color_map = cm.linear.Set1_07.to_step(len(bins),index=[i for i in range (len(bins))])
+
+    # The function takes three parameters:
+    # - bins: bins list above cutoff after running pipeline.remove_noise
+    # - bins_list: the list of indices of selected bins
+    # - trips: the trips after running pipeline.read_data
+
+    # In order to plot trips correctly, map index should be len(bins)+1,
+    # so that the color steps length is equal to the bins number. For bin index which is 0,
+    # will be plotted in color which is in index 1.
+    color_map = cm.linear.Set1_07.to_step(len(bins)+1, index=[i for i in range(len(bins)+1)])
     map = folium.Map(location=[trips[0].data.start_loc["coordinates"][1], trips[0].data.start_loc["coordinates"][0]],
                    zoom_start=12, max_zoom=30, control_scale=True)
-    for index, curr_bin in enumerate(bins):
-        for curr_trip_index in curr_bin:
-            curr_trip = trips[curr_trip_index]
-            # We need polyline to plot the trip according to start_loc and end_loc
-            # Flip indices because points are in geojson (i.e. lon, lat),folium takes [lat,lon]
-            layer = folium.PolyLine(
-                [[curr_trip.data.start_loc["coordinates"][1], curr_trip.data.start_loc["coordinates"][0]],
-                 [curr_trip.data.end_loc["coordinates"][1], curr_trip.data.end_loc["coordinates"][0]]], weight=2,
-                color=color_map(index))
-            layer.add_to(map)
+
+    if bins_list:
+        for bin_index in bins_list:
+            curr_bin = bins[bins_list[bin_index]]
+            map_index = bins_list[bin_index]+1
+            for curr_trip_index in curr_bin:
+                curr_trip = trips[curr_trip_index]
+                # We need polyline to plot the trip according to start_loc and end_loc
+                # Flip indices because points are in geojson (i.e. lon, lat),folium takes [lat,lon]
+                layer = folium.PolyLine(
+                    [[curr_trip.data.start_loc["coordinates"][1], curr_trip.data.start_loc["coordinates"][0]],
+                     [curr_trip.data.end_loc["coordinates"][1], curr_trip.data.end_loc["coordinates"][0]]], weight=2,
+                    color=color_map(map_index))
+                layer.add_to(map)
+
+    else:
+        for index, curr_bin in enumerate(bins):
+            for curr_trip_index in curr_bin:
+                curr_trip = trips[curr_trip_index]
+                layer = folium.PolyLine(
+                    [[curr_trip.data.start_loc["coordinates"][1], curr_trip.data.start_loc["coordinates"][0]],
+                     [curr_trip.data.end_loc["coordinates"][1], curr_trip.data.end_loc["coordinates"][0]]], weight=2,
+                    color=color_map(index))
+                layer.add_to(map)
     map.add_child(color_map)
     return map
 
 
-# def cutoff_bins_map(bins, trips):
-#     color_map = cm.linear.Set1_07.to_step(len(bins), index=[i for i in range(len(bins))])
-#     map = folium.Map(
-#         location=[trips[0].data.start_loc["coordinates"][1], trips[0].data.start_loc["coordinates"][0]],
-#         zoom_start=12, max_zoom=30, control_scale=True)
-#     for t, bin in enumerate(bins):
-#         for trip_index in bin:
-#             # curr_trip = bin_trips[curr_trip_index]
-#             curr_trip = trips[trip_index]
-#             layer = folium.PolyLine(
-#                 [[curr_trip.data.start_loc["coordinates"][1], curr_trip.data.start_loc["coordinates"][0]],
-#                  [curr_trip.data.end_loc["coordinates"][1], curr_trip.data.end_loc["coordinates"][0]]], weight=2,
-#                 color=color_map(t))
-#             layer.add_to(map)
-#     map.add_child(color_map)
-#     return map
-
-
-def clusters_map(labels, points, clusters):
+def clusters_map(labels,labels_list, points, clusters):
     # Each color represents a label
     # labels have to be in order in the colormap index
-    labels_clt = list(set(sorted(labels)))
-    color_map = cm.linear.Set1_07.to_step(clusters, index=[i for i in labels_clt])
 
     # Plot all clusters with different colors on the map
     # Choose the first start point to locate the map
+
+    # The function takes four parameters:
+    # - labels: trip label list after running featurization/pipeline.cluster
+    # - labels_list: the list of selected label
+    # - points: coordinate points from featurization/pipeline.cluster
+    # - clusters: number of clusters
     map = folium.Map(location=[points[0][1], points[0][0]], zoom_start=12, max_zoom=30, control_scale=True)
-
-    if labels:
-        for i, point in enumerate(points):
-            start_lat = point[1]
-            start_lon = point[0]
-            end_lat = point[3]
-            end_lon = point[2]
-            layer = folium.PolyLine([[start_lat, start_lon],
-                                     [end_lat, end_lon]], weight=2, color=color_map(labels[i]))
-            layer.add_to(map)
-    map.add_child(color_map)
-    return map
-
-
-def specific_bin_map(bins, trips):
-    color_map = cm.linear.Set1_07.to_step(len(bins), index=[i for i in range(len(bins))])
-    # Plot trips in the same bin,
-    map = folium.Map(
-        location=[trips[0].data.start_loc["coordinates"][1], trips[0].data.start_loc["coordinates"][0]],
-        zoom_start=12, max_zoom=30, control_scale=True)
-
-    # Here we to choose a specific bin
-    for t in range(0, 3):
-        for i in range(len(bins[t])):
-            curr_trip = trips[bins[t][i]]
-            layer = folium.PolyLine(
-                [[curr_trip.data.start_loc["coordinates"][1], curr_trip.data.start_loc["coordinates"][0]],
-                 [curr_trip.data.end_loc["coordinates"][1], curr_trip.data.end_loc["coordinates"][0]]], weight=2,
-                color=color_map(t))
-
-            layer.add_to(map)
-    map.add_child(color_map)
-    return map
-
-
-def specific_cluster_map(labels, points, clusters):
     labels_clt = list(set(sorted(labels)))
-    color_map = cm.linear.Set1_07.to_step(clusters, index=[i for i in labels_clt])
+    color_map = cm.linear.Set1_07.to_step(clusters + 1, index=[i for i in range(len(labels_clt) + 1)])
 
-    # Plot all clusters with different colors on the map
-    # Choose the first start point to locate the map
-    map = folium.Map(location=[points[0][1], points[0][0]], zoom_start=12, max_zoom=30,
-                             control_scale=True)
-
-    if labels:
-        for i, point in enumerate(points):
-            # Here we can choose a specific cluster to plot
-            if labels[i] == 1:
+    if labels_list:
+        for label in labels_list:
+            for i, point in enumerate(points):
+                if labels[i] == label:
+                    start_lat = point[1]
+                    start_lon = point[0]
+                    end_lat = point[3]
+                    end_lon = point[2]
+                    layer = folium.PolyLine([[start_lat, start_lon],
+                                             [end_lat, end_lon]], weight=2, color=color_map(labels[i]+1))
+                    layer.add_to(map)
+    else:
+        if labels:
+            for i, point in enumerate(points):
                 start_lat = point[1]
                 start_lon = point[0]
                 end_lat = point[3]
                 end_lon = point[2]
                 layer = folium.PolyLine([[start_lat, start_lon],
-                                         [end_lat, end_lon]], weight=2, color=color_map(labels[i]))
+                                         [end_lat, end_lon]], weight=2, color=color_map(labels[i]+1))
                 layer.add_to(map)
     map.add_child(color_map)
     return map
