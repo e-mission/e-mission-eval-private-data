a00d03c473af354e2a29665fd4bf29b81eccf4cd
Chunrui Huang <68228318+corinne-hcr@users.noreply.github.com>
2021-05-29 17:50:43 -0700

    Update tour_model_eval/get_scores.py
    Co-authored-by: shankari <shankari@eecs.berkeley.edu>

--- tour_model_eval/get_scores.py
+++ tour_model_eval/get_scores.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pandas.testing import assert_frame_equal
 import label_processing as label_pro
-from sklearn import metrics
+import sklearn.metrics as skm
 import itertools
 
 
